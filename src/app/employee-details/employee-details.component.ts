435c7dd520590c810ff6c73eb469131e88f0e754
Asad Ullah Jamil Ch <asadullahjamilchaudhry@gmail.com>
2025-01-14 22:32:03 +0500

    just correct minor mistake.

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -22,7 +22,7 @@ export class EmployeeDetailsComponent {
       this.empId = id;
     })
   }
-  nextMap() {
+  nextEmp() {
     let nextId = parseInt(this.empId) + 1;
     this.router.navigate(['/employee/', nextId])
   }
